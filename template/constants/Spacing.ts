0a427477ec215bc0bd7959e1d36701329373b2e9
Ali Maher <ali.maher.mohammed@gmail.com>
2026-06-28 15:30:15 +0300

    feat: add Spacing constant definitions with moderate scaling support

---
+++ template/constants/Spacing.ts
@@ -0,0 +1,34 @@
+import { moderateScale } from './Metrics';
+
+const Spacing = {
+  /** x1 = 4px */
+  x1: moderateScale(4),
+  /** x2 = 8px */
+  x2: moderateScale(8),
+  /** x3 = 12px */
+  x3: moderateScale(12),
+  /** x4 = 16px */
+  x4: moderateScale(16),
+  /** x5 = 20px */
+  x5: moderateScale(20),
+  /** x6 = 24px */
+  x6: moderateScale(24),
+  /** x7 = 28px */
+  x7: moderateScale(28),
+  /** x8 = 32px */
+  x8: moderateScale(32),
+  /** x9 = 36px */
+  x9: moderateScale(36),
+  /** x10 = 40px */
+  x10: moderateScale(40),
+  /** x11 = 44px */
+  x11: moderateScale(44),
+  /** x12 = 48px */
+  x12: moderateScale(48),
+  /** x13 = 52px */
+  x13: moderateScale(52),
+  /** x14 = 56px */
+  x14: moderateScale(56),
+};
+
+export default Spacing;
